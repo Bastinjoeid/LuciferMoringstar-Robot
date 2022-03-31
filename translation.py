36586019f1194cbc7f2410db9db1eb1f7c904545
Bastinjoeid <102047556+Bastinjoeid@users.noreply.github.com>
2022-03-31 04:02:42 -0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,35 +1,33 @@
 class LuciferMoringstar(object):
 
-    DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
+    DEFAULT_MSG = """👋ʜᴇʟʟᴏ {ᴍᴇɴᴛɪᴏɴ}.....!!!\ɴɪᴛ's ᴘᴏᴡᴇʀ ғᴜʟʟ [{ʙᴏᴛ_ɴᴀᴍᴇ}](ᴛ.ᴍᴇ/{ʙᴏᴛ_ᴜsᴇʀɴᴀᴍᴇ}) ʜᴇʀᴇ 😎\ɴᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ sᴜʀᴇ ɪ'ᴍ ᴀɴ ᴀᴅᴍɪɴ ᴛʜᴇʀᴇ! \ɴᴀɴᴅ ᴇɴᴊᴏʏ ᴍʏ ᴘᴇᴠᴇʀ sʜᴏᴡ.....!!!🤪"""
 
     HELP_MSG = """**Help Veno Monuse 🤣**"""
 
     ABOUT_MSG = """
 🤖 My Name [{bot_name}](t.me/{bot_username})
 
-🧑 My Dev : [PR0FESS0R-99](t.me/PR0FESS0R_99)
+🧑 My Dev : [Sovel_jaison](t.me/Sovel_jaison)
 
-🧑 My Creator : {dev_name}
+🧑 My Creator : {dev_name}(https://t.me/Sovel_jaison)
 
-📦 My Source : [Click Here](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+📦 My Source : [Click Here](https://t.me/cinemahub00)
 
-📺 My Tutorial : [Ckick Here](https://youtu.be/FCU_XJmyG_U)
+🗃️ My Library : [Pyrogram]
 
-🗃️ My Library : [Pyrogram](https://youtu.be/Af055Eozk9s)
+🖇️ My Language : [Python3]
 
-🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s)
+🗃️ My Database : [Mongodb]
 
-🗃️ My Database : [Mongodb](https://youtu.be/gBLTsH-IXr0)
+🗳️ My Server : [Heroku]
 
-🗳️ My Server : [Heroku](https://youtu.be/xcZD55td6Vw)
+🏷️ My Updates : [Movie updates](t.me/cinemahub1000)
 
-🏷️ My Updates : [Mo Tech](t.me/Mo_Tech_YT)
-
-🗣️ My Support : [Any Doubt](t.me/Mo_Tech_Group)"""
+🗣️ My Support : [Any Doubt](t.me/cinemahub00)"""
 
     FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
 
-    PR0FESS0R_99 = """
+    Sovel_jaison = """
 **ADMINS COMMANDS**
 
  » /broadcast - Reply Any Media Or Message
